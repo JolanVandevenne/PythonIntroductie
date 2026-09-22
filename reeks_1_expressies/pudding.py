8d77d5e8007a6028c434e510805916beb9eecd7f
Jolan <jolan.vandevenne@student.hogent.be>
2026-09-22 21:22:41 +0200

    PogingPuddingOefening

---
+++ reeks_1_expressies/pudding.py
@@ -0,0 +1,9 @@
+aantalStuks = int(input())
+kostprijsStuk = float(input())
+aantalBarcodes = int(input())
+aantalMijnen = int(input())
+
+gespendeerdeDollars = aantalStuks * kostprijsStuk
+aantalFrequentFlyerMijlen = (aantalStuks / aantalBarcodes) * aantalMijnen
+
+print(f"Phillips spendeerde ${gespendeerdeDollars} voor {aantalFrequentFlyerMijlen} frequent flyer mijlen.")
