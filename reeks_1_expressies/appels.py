efa4789bf69b0287828906ff67b736fe57db3c2c
Jolan <jolan.vandevenne@student.hogent.be>
2026-09-24 20:29:32 +0200

    AppelsOefening

--- reeks_1_expressies/appels.py
+++ reeks_1_expressies/appels.py
@@ -1,7 +1,15 @@
+import math
+
 maxAppelsKist = 20
 maxKistenPallet = 35
 aantalAppelsOpPallet = maxAppelsKist * maxKistenPallet
 
 aantalAppels = int(input())
-print(aantalAppels // aantalAppelsOpPallet)
-print(maxAppelsKist // (aantalAppels // aantalAppelsOpPallet))
+
+hoeveelVolledigePallets = aantalAppels // aantalAppelsOpPallet
+hoeveelOverigeVolleKisten = math.floor((aantalAppels % aantalAppelsOpPallet) / maxAppelsKist)
+hoeveelAppelsOver = (aantalAppels % maxAppelsKist)
+
+print(hoeveelVolledigePallets)
+print(hoeveelOverigeVolleKisten)
+print(hoeveelAppelsOver)
